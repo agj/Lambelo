3fdc422e3c2c28cd88f756bb50a1cefdcf4d200d
agj <agj_chile@hotmail.com>
2014-09-23 23:40:01 -0300

    Refactoring. Now variables don't leak.

--- lambelo.php
+++ lambelo.php
@@ -10,80 +10,77 @@ class L {
 	static $fns;
 }
 
-$autoCurry = function ($fn) use (&$autoCurryTo) {
-	$ref = new ReflectionFunction($fn);
-	$arity = $ref->getNumberOfRequiredParameters();
-	return $autoCurryTo($arity, $fn);
-};
+call_user_func( function () { // Creating a closure to prevent variable leakage.
 
-$autoCurryTo = function ($arity, $fn) use (&$autoCurryTo) {
-	return function () use ($fn, $arity, $autoCurryTo) {
-		$numArgs = func_num_args();
-		if ($numArgs >= $arity) {
-			return call_user_func_array($fn, func_get_args());
-		} else {
-			$args = func_get_args();
-			return $autoCurryTo($arity - $numArgs, function () use ($fn, $args) {
-				return call_user_func_array($fn, array_merge($args, func_get_args()));
-			});
-		}
+	$autoCurryTo = function ($arity, $fn) use (&$autoCurryTo) {
+		return function () use ($fn, $arity, $autoCurryTo) {
+			$numArgs = func_num_args();
+			if ($numArgs >= $arity) {
+				return call_user_func_array($fn, func_get_args());
+			} else {
+				$args = func_get_args();
+				return $autoCurryTo($arity - $numArgs, function () use ($fn, $args) {
+					return call_user_func_array($fn, array_merge($args, func_get_args()));
+				});
+			}
+		};
 	};
-};
 
-$ascSort = function ($left, $right) {
-	if ($left === $right) return 0;
-	if ($left > $right)   return -1;
-	if ($left < $right)   return 1;
-};
+	$autoCurry = function ($fn) use ($autoCurryTo) {
+		$ref = new ReflectionFunction($fn);
+		$arity = $ref->getNumberOfRequiredParameters();
+		return $autoCurryTo($arity, $fn);
+	};
 
-L::$fns = array(
-	'autoCurry' => $autoCurry,
+	$ascSort = function ($left, $right) {
+		if ($left === $right) return 0;
+		if ($left > $right)   return -1;
+		if ($left < $right)   return 1;
+	};
 
-	'autoCurryTo' => $autoCurryTo,
-
-	'map' => $autoCurry( function ($fn, $obj) {
+	$map = function ($fn, $obj) {
 		return array_map($fn, $obj);
-	}),
+	};
 
-	'reduce' => $autoCurry( function ($fn, $obj, $initial = null) {
+	$reduce = function ($fn, $obj, $initial = null) {
 		if (is_null($initial)) return array_reduce($obj, $fn);
 		return array_reduce($obj, $fn, $initial);
-	}),
+	};
 
-	'filter' => $autoCurry( function ($fn, $obj) {
+	$filter = function ($fn, $obj) {
 		return array_filter($obj, $fn);
-	}),
+	};
 
-	'prop' => $autoCurry( function ($prop, $obj) {
+	$prop = function ($prop, $obj) {
 		return $obj[$prop];
-	}),
+	};
 
-	'flatten' => $autoCurry( function ($obj) {
+	$flatten = function ($obj) {
 		return L::flattenTo(999999, $obj);
-	}),
+	};
 
-	'flattenTo' => $autoCurry( function ($depth, $obj) {
+	$flattenTo = function ($depth, $obj) {
 		if ($depth <= 0) return $obj;
 		return L::reduce( function ($memo, $item) {
 			if (is_array($item)) return array_merge($memo, L::flattenTo($depth - 1, $item));
 			$memo[] = $item;
 			return $memo;
 		}, $obj, array());
-	}),
+	};
 
-	'unique' => $autoCurry( function ($arr) {
+	$unique = function ($arr) {
 		return L::reduce( function ($memo, $item) {
 			if (in_array($item, $memo)) return $memo;
 			$memo[] = $item;
 			return $memo;
 		}, $arr, array());
-	}),
+	};
 
-	'sort' => $autoCurry( function ($arr) use ($ascSort) {
+	$sort = function ($arr) use ($ascSort) {
 		return L::sortBy($ascSort, $arr);
-	}),
+	};
 
-	'sortBy' => $autoCurry( function ($fn, $arr) {
+	$sortBy = function ($fn, $arr) {
 		$len = count($arr);
 		if ($len <= 1) return $arr;
 		$pivot = $arr[0];
@@ -96,23 +93,47 @@ L::$fns = array(
 			else $left[] = $item;
 		}
 		return array_merge(L::sortBy($fn, $left), array($pivot), L::sortBy($fn, $right));
-	}),
+	};
 
-	'sequence' => $autoCurryTo(1, function () {
+	$sequence = function () {
 		$fns = func_get_args();
 		return function () use ($fns) {
 			return L::reduce( function ($memo, $fn) {
 				return $fn($memo);
 			}, array_slice($fns, 1), call_user_func_array($fns[0], func_get_args()));
 		};
-	}),
+	};
 
-	'call' => $autoCurryTo(2, function ($fn) {
+	$call =function ($fn) {
 		$args = array_slice(func_get_args(), 1);
 		return call_user_func_array($fn, $args);
-	}),
+	};
 
-	'apply' => $autoCurry( function ($fn, $args) {
+	$apply = function ($fn, $args) {
 		return call_user_func_array($fn, $args);
-	}),
-);
+	};
+
+	$equals = function ($a, $b) {
+		return $a === $b;
+	};
+
+
+	L::$fns = array(
+		'autoCurry'   => $autoCurry,
+		'autoCurryTo' => $autoCurryTo,
+		'map'         => $autoCurry($map),
+		'reduce'      => $autoCurry($reduce),
+		'filter'      => $autoCurry($filter),
+		'prop'        => $autoCurry($prop),
+		'flatten'     => $autoCurry($flatten),
+		'flattenTo'   => $autoCurry($flattenTo),
+		'unique'      => $autoCurry($unique),
+		'sort'        => $autoCurry($sort),
+		'sortBy'      => $autoCurry($sortBy),
+		'sequence'    => $autoCurryTo(1, $sequence),
+		'call'        => $autoCurryTo(2, $call),
+		'apply'       => $autoCurry($apply),
+		'equals'      => $autoCurry($equals),
+	);
+
+});
